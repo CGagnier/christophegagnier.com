33c0114693f4072fdca661d9ac617043c7b6c5d6
Christophe Gagnier <c.gagnier11@gmail.com>
2020-02-08 20:26:44 -0500

    :fire: Clean theme asset

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,10 @@ require(`dotenv`).config({
 
 module.exports = {
   siteMetadata: {
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
+    siteTitleAlt: `Christophe Gagnier - Blog`,
+    siteTitle: `Christophe Gagnier`,
+    author: `@CGagnier`,
+    siteUrl: `https://christophegagnier.com`,
   },
   plugins: [
     {
@@ -23,11 +26,15 @@ module.exports = {
         externalLinks: [
           {
             name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
+            url: `https://twitter.com/CGagnier`,
           },
           {
             name: `Instagram`,
-            url: `https://www.instagram.com/lekoarts.de/`,
+            url: `https://www.instagram.com/cgagnier11/`,
+          },
+          {
+            name: `LinkedIn`,
+            url: `https://www.linkedin.com/in/christophegagnier/`,
           },
         ],
       },
@@ -42,12 +49,12 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        name: `Christophe Gagnier - Blog`,
+        short_name: `blog`,
+        description: `Personnal blog.`,
         start_url: `/`,
         background_color: `#fff`,
-        theme_color: `#6B46C1`,
+        theme_color: `#ffba5a`,
         display: `standalone`,
         icons: [
           {
